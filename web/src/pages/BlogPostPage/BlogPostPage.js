209dd8f1d9aa546406774f442108d1d7e54c8131
Oneted11 <muriithited@gmail.com>
2020-08-25 09:02:39 +0300

    err:no id being passed to blogPostquesry

--- web/src/pages/BlogPostPage/BlogPostPage.js
+++ web/src/pages/BlogPostPage/BlogPostPage.js
@@ -1,14 +1,9 @@
 import { Link, routes } from '@redwoodjs/router'
-
+import BlogPostCell from 'src/components/BlogPostCell'
 const BlogPostPage = () => {
   return (
     <>
-      <h1>BlogPostPage</h1>
-      <p>Find me in "./web/src/pages/BlogPostPage/BlogPostPage.js"</p>
-      <p>
-        My default route is named "blogPost", link to me with `
-        <Link to={routes.blogPost()}>BlogPost</Link>`
-      </p>
+      <BlogPostCell />
     </>
   )
 }
