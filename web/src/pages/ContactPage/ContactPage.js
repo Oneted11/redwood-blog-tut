f47c33b3eacbed0470614cacff6ac2e10a192e3c
Oneted11 <muriithited@gmail.com>
2020-09-19 16:17:40 +0300

    added contact page & validation

--- web/src/pages/ContactPage/ContactPage.js
+++ web/src/pages/ContactPage/ContactPage.js
@@ -1,15 +1,57 @@
 import { Link, routes } from '@redwoodjs/router'
+import BlogLayout from 'src/layouts/BlogLayout/BlogLayout'
+import {
+  Form,
+  TextField,
+  Submit,
+  TextAreaField,
+  Label,
+  FieldError,
+} from '@redwoodjs/forms'
 
 const ContactPage = () => {
+  const onSubmit = (data) => {
+    console.log(data)
+  }
   return (
-    <>
-      <h1>ContactPage</h1>
-      <p>Find me in "./web/src/pages/ContactPage/ContactPage.js"</p>
-      <p>
-        My default route is named "contact", link to me with `
-        <Link to={routes.contact()}>Contact</Link>`
-      </p>
-    </>
+    <BlogLayout>
+      <Form onSubmit={onSubmit}>
+        <Label name="name" errorClassName="error">
+          {' '}
+          Name
+        </Label>
+        <TextField
+          name="name"
+          placeHolder="Add name"
+          validation={{ required: true }}
+          errorClassName="error"
+        />
+        <FieldError name="name" className="error" />
+
+        <Label name="email" errorClassName="error">
+          Email
+        </Label>
+        <TextField
+          name="email"
+          placeHolder="add email"
+          validation={{ required: true }}
+          errorClassName="error"
+        />
+        <FieldError name="email" className="error" />
+
+        <Label name="message">Message</Label>
+
+        <TextAreaField
+          name="message"
+          placeHolder="add text"
+          validation={{ required: true }}
+          errorClassName="error"
+        />
+        <FieldError name="message" className="error" />
+
+        <Submit>Save</Submit>
+      </Form>
+    </BlogLayout>
   )
 }
 
